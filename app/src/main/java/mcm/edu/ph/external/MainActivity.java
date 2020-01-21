6c1b8693cdec21f9027c9459770012c822edfbe0
Agraeth <rw.lee36@gmail.com>
2020-01-21 10:33:02 +0800

    Imported code from the Internal Project including XML.

--- app/src/main/java/mcm/edu/ph/external/MainActivity.java
+++ app/src/main/java/mcm/edu/ph/external/MainActivity.java
@@ -1,14 +1,92 @@
 package mcm.edu.ph.external;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import android.widget.Toast;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
 import android.os.Bundle;
 
+
+
 public class MainActivity extends AppCompatActivity {
+
+    static final int READ_BLOCK_SIZE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText txtbox = findViewById(R.id.txtbox);
+        final Button btnClear = findViewById(R.id.btnClear);
+        final Button btnRead = findViewById(R.id.btnRead);
+        final Button btnWrite = findViewById(R.id.btnWrite);
+
+        btnClear.setOnClickListener (
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        txtbox.setText("");
+                    }
+                }
+        );
+        btnWrite.setOnClickListener(
+                new View.OnClickListener () {
+                    @Override
+                    public void onClick(View v) {
+                        try {
+                            FileOutputStream fileout=openFileOutput("mytextfile.txt", MODE_PRIVATE);
+                            OutputStreamWriter outputWriter=new OutputStreamWriter(fileout);
+                            outputWriter.write(txtbox.getText().toString());
+                            outputWriter.close();
+
+                            //display file saved message
+                            Toast.makeText(getBaseContext(), "File saved successfully!",
+                                    Toast.LENGTH_SHORT).show();
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }
+        );
+        btnRead.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        try {
+                            FileInputStream fileIn=openFileInput("mytextfile.txt");
+                            InputStreamReader InputRead= new InputStreamReader(fileIn);
+
+                            char[] inputBuffer= new char[READ_BLOCK_SIZE];
+                            String s="";
+                            int charRead;
+
+                            while ((charRead=InputRead.read(inputBuffer))>0) {
+                                // char to string conversion
+                                String readstring=String.copyValueOf(inputBuffer,0,charRead);
+                                s +=readstring;
+                            }
+                            InputRead.close();
+                            txtbox.setText(s);
+
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+
+
     }
+
 }
+
+
